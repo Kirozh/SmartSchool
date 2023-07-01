27295196fb1d63643d1a51ba9781f8ac1bf0a5ca
Kirill Ozhigin <cuprum.oxigen@gmail.com>
2023-07-01 22:15:06 +0300

    Initial commit

---
+++ app/src/main/java/com/kirozh/smartschool/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.kirozh.smartschool
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
