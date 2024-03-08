bb7a5a00d32c4dd9423861318b1194f4d7222256
Kirill Ozhigin <cuprum.oxigen@gmail.com>
2024-03-08 18:08:09 +0300

    Bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 10
-        versionName = "0.1.0"
+        versionName = "0.2.0"
         multiDexEnabled = true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
